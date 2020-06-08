42576b309111cbe411f40efae9a92fae46cbae99
Tyler Williams <tyler.williams.147@gmail.com>
2020-06-08 11:25:55 -0500

    OpenProject() now uses revisions in all calls

--- NatoliOrderInterface/ProjectWindow.xaml.cs
+++ NatoliOrderInterface/ProjectWindow.xaml.cs
@@ -5325,6 +5325,7 @@ namespace NatoliOrderInterface
                     }
                     else
                     {
+                        MultiTipSketchViewer.Source = null;
                         using var projectsContext = new ProjectsContext();
                         EngineeringProjects project = projectsContext.EngineeringProjects.First(p => p.ProjectNumber == projectNumber && p.RevNumber == projectRevNumber);
                         projectsContext.EngineeringProjects.Remove(project);
