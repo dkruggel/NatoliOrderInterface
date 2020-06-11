32ad7514c9a513a9472f190ab5b4cb2e1611eed9
Tyler Williams <tyler.williams.147@gmail.com>
2020-06-11 14:49:13 -0500

    Test Wrap

---
+++ NatoliOrderInterface/WrapTesting.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace NatoliOrderInterface
+{
+    /// <summary>
+    /// Interaction logic for WrapTesting.xaml
+    /// </summary>
+    public partial class WrapTesting : Window
+    {
+        public WrapTesting()
+        {
+            InitializeComponent();
+        }
+    }
+}
